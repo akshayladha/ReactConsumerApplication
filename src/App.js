6ed7701b7ec59f03dcb9b42239fe850a4ce021e6
Akshay Ladha <akshay.ladha@capita.co.uk>
2020-09-11 15:36:12 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import * as akshay from 'web-components-library-akshay-test/src/demo/index';
+
+function App() {
+  return (
+    <akshay class="akshay" count="10"></akshay>
+  );
+}
+
+export default App;
